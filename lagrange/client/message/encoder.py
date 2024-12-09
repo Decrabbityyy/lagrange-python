65a9970998c1f0d5215ee3c97761b5d63e6c1e12
Decrabbit <decrabbit@163.com>
2024-12-09 21:07:39 +0800

    fix: 修复类型检查以支持延迟导入 Client 类

--- lagrange/client/message/encoder.py
+++ lagrange/client/message/encoder.py
@@ -4,10 +4,10 @@ import random
 import struct
 from uuid import uuid4
 import zlib
-from typing import Any, Callable, Optional
+from typing import TYPE_CHECKING, Any, Callable, Optional
 from collections.abc import Coroutine
-
-from lagrange.client.client import Client
+if TYPE_CHECKING:
+    from lagrange.client.client import Client
 from lagrange.pb.message.heads import ContentHead, Forward, ResponseHead
 from lagrange.pb.message.longmsg import LongMsgAction, LongMsgActionBody, LongMsgRespResult, LongMsgResult, LongMsgRsp
 from lagrange.pb.message.msg import Message
@@ -273,7 +273,7 @@ async def build_forward_msg(
 
 #it should be enterpoint
 async def _get_mulitmsg_resid(
-    client: Client, forword_msg: MulitMsg, target: str = "", grp_id: Optional[int] = None
+    client: "Client", forword_msg: MulitMsg, target: str = "", grp_id: Optional[int] = None
 ) -> str:
     body = await build_forward_msg(forword_msg, get_resid_func(client, target, grp_id))
     packet = await client.send_uni_packet(
@@ -285,7 +285,7 @@ async def _get_mulitmsg_resid(
 
 
 def get_resid_func(
-    client: Client, target: str = "", grp_id: Optional[int] = None
+    client: "Client", target: str = "", grp_id: Optional[int] = None
 ) -> Callable[..., Coroutine[Any, Any, str]]:
     async def wrap(forword_msg: MulitMsg):
         return await _get_mulitmsg_resid(client, forword_msg, target, grp_id)
